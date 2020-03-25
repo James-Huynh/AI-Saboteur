ef98ba3edb129b5fb1e86530f52806313f07f24f
tuguldur <tuguldur@192.168.2.159>
2020-03-25 19:48:55 -0400

    implemented getPathMoves

--- SaboteurComp424/src/student_player/MyTools.java
+++ SaboteurComp424/src/student_player/MyTools.java
@@ -1,7 +1,30 @@
 package student_player;
 
+import java.util.ArrayList;
+
+import Saboteur.SaboteurMove;
+
 public class MyTools {
     public static double getSomething() {
         return Math.random();
     }
+    
+    
+    /**
+     * @param moves = returned ArrayList of all possible moves from the getAllLegalMoves method
+     * @return ArrayList of only tile moves filtered from the input of all possible moves
+     */
+    public static ArrayList<SaboteurMove> getPathMoves(ArrayList<SaboteurMove> moves) {
+    	ArrayList<SaboteurMove> returnVal = new ArrayList<SaboteurMove>();
+    	int moves_size = moves.size();
+    	
+    	for(int i=0; i < moves_size; i++) {
+    		if(moves.get(i).getCardPlayed().getName().matches("Tile(.*)")) {
+    			returnVal.add(moves.get(i));
+    		}
+    	}
+    	return returnVal;
+    }
+    
+    
 }
